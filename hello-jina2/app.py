19adc9680252011a0e1b8104c80be88fa07762d5
wangruya <wangruya@wangruyadeMacBook-Pro.local>
2022-05-20 17:56:28 +0800

    first commit

---
+++ hello-jina2/app.py
@@ -0,0 +1,64 @@
+from docarray import Document
+from jina import Flow, DocumentArray
+import os
+import glob
+from jina.types.request.data import DataRequest
+
+def config():
+    os.environ['JINA_PORT'] = '45679'  # the port for accessing the RESTful service, i.e. http://localhost:45678/docs
+    os.environ['JINA_WORKSPACE'] = './workspace'  # the directory to store the indexed data
+    os.environ['TOP_K'] = '20'  # the maximal number of results to return
+
+def get_docs(data_path):
+    for fn in glob.glob(os.path.join(data_path, '*.mp4')):
+        yield Document(uri=fn, id=os.path.basename(fn))
+
+def check_index(resp: DataRequest):
+    print(resp.data)
+    for doc in resp.docs:
+        print(f'chunks: {len(doc.chunks)}')
+
+def check_search(resp: DataRequest):
+    for doc in resp.docs:
+        print(f'Query text: {doc.text}')
+        print(f'Matches: {len(doc.matches)}')
+        for m in doc.matches:
+            print(m)
+            print(f'+- id: {m.id}, score: {m.scores["cosine"].value}, timestampe: {m.tags["timestamp"]}')
+        print('-'*10)
+
+config()
+
+f = Flow().add(
+    uses='videoLoader/config.yml',uses_requests={"/index": "extract"}, name="video_loader"
+    ).add(
+        uses="customClipImage/config.yml",
+        name="image_encoder",
+        uses_requests={"/index": "encode"}
+    ).add(
+        uses="customClipText/config.yml",
+        name="text_encoder",
+    ).add(
+        uses="indexer/config.yml",
+        name="indexer",
+        uses_metas={"workspace": os.environ['JINA_WORKSPACE']}
+    ).add(
+        uses="ranker/config.yml",
+        name="ranker",
+    )
+
+
+with f:
+    f.post(
+        '/index', 
+        inputs=get_docs('toy_data'),
+        on_done=check_index
+        )
+    a = f.post(
+                on='/search',
+                inputs=DocumentArray([
+                    Document(text='a girl'),
+                    Document(text='sports bracelet'),
+                    Document(text='Sports Shoes'),
+                ]),
+                on_done=check_search)
---
+++ hello-jina2/clipText/clip_text.py
@@ -0,0 +1,93 @@
+from typing import Dict, Optional, Sequence
+
+import torch
+from docarray import DocumentArray
+from jina import Executor, requests
+from transformers import CLIPModel, CLIPTokenizer
+
+
+class CLIPTextEncoder(Executor):
+    """Encode text into embeddings using the CLIP model."""
+
+    def __init__(
+        self,
+        pretrained_model_name_or_path: str = 'openai/clip-vit-base-patch32',
+        base_tokenizer_model: Optional[str] = None,
+        max_length: int = 77,
+        device: str = 'cpu',
+        traversal_paths: str = '@r',
+        batch_size: int = 32,
+        *args,
+        **kwargs,
+    ):
+        """
+        :param pretrained_model_name_or_path: Can be either:
+            - A string, the model id of a pretrained CLIP model hosted
+                inside a model repo on huggingface.co, e.g., 'openai/clip-vit-base-patch32'
+            - A path to a directory containing model weights saved, e.g., ./my_model_directory/
+        :param base_tokenizer_model: Base tokenizer model.
+            Defaults to ``pretrained_model_name_or_path`` if None
+        :param max_length: Max length argument for the tokenizer.
+            All CLIP models use 77 as the max length
+        :param device: Pytorch device to put the model on, e.g. 'cpu', 'cuda', 'cuda:1'
+        :param traversal_paths: Default traversal paths for encoding, used if
+            the traversal path is not passed as a parameter with the request.
+        :param batch_size: Default batch size for encoding, used if the
+            batch size is not passed as a parameter with the request.
+        """
+        super().__init__(*args, **kwargs)
+        self.traversal_paths = traversal_paths
+        self.batch_size = batch_size
+        self.pretrained_model_name_or_path = pretrained_model_name_or_path
+        self.base_tokenizer_model = (
+            base_tokenizer_model or pretrained_model_name_or_path
+        )
+        self.max_length = max_length
+
+        self.device = device
+        self.tokenizer = CLIPTokenizer.from_pretrained(self.base_tokenizer_model)
+        self.model = CLIPModel.from_pretrained(self.pretrained_model_name_or_path)
+        self.model.eval().to(device)
+
+    @requests
+    def index(self, docs: DocumentArray, parameters: Dict, **kwargs):
+        print('clip_text index')
+
+    @requests
+    def encode(self, docs: DocumentArray, parameters: Dict, **kwargs):
+        """
+        Encode all documents with the `text` attribute and store the embeddings in the
+        `embedding` attribute.
+
+        :param docs: DocumentArray containing the Documents to be encoded
+        :param parameters: A dictionary that contains parameters to control encoding.
+            The accepted keys are ``traversal_paths`` and ``batch_size`` - in their
+            absence their corresponding default values are used.
+        """
+        print('clip_text encode')
+        for docs_batch in DocumentArray(
+            filter(
+                lambda x: bool(x.text),
+                docs[parameters.get('traversal_paths', self.traversal_paths)],
+            )
+        ).batch(batch_size=parameters.get('batch_size', self.batch_size)) :
+
+            text_batch = docs_batch.texts
+
+            with torch.inference_mode():
+                input_tokens = self._generate_input_tokens(text_batch)
+                embeddings = self.model.get_text_features(**input_tokens).cpu().numpy()
+                for doc, embedding in zip(docs_batch, embeddings):
+                    doc.embedding = embedding
+
+    def _generate_input_tokens(self, texts: Sequence[str]):
+
+        input_tokens = self.tokenizer(
+            texts,
+            max_length=self.max_length,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt',
+        )
+        input_tokens = {k: v.to(self.device) for k, v in input_tokens.items()}
+        return input_tokens
